be2b5390a9914a038faa1a31e7eeb644d7879530
JoseType <jomartgo22@gmail.com>
2025-10-09 13:31:31 -0300

    ACT

--- src/pages/ConfirmationPurchase/ConfirmationPurchase.jsx
+++ src/pages/ConfirmationPurchase/ConfirmationPurchase.jsx
@@ -1,8 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ConfirmationPurchaseimport() {
+
+    const BASE_URL = import.meta.env.VITE_SERVER_API;
+
+    const [ orderBD, setOrderBD ] = useState(null)
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const collectionId = queryParams.get("collection_id");
+    const status = queryParams.get("collection_status"); // o "status"
+
+    const getOrders = async() => {
+
+        try{
+
+            const response = await axios.get(`${BASE_URL}/confirmationPurchase?collection_id=${collectionId}&collection_status=${status}`);
+            setOrderBD(response.data.order)
+
+        }catch(err){
+            console.log(err);
+        }
+        }
+
+        useEffect( () => {
+            getOrders();
+        }, [])
 
     return(
         <>
-            <p>Hola esta es la pagina de confirmacion de compra</p>
+            <h2>Confirmación de Compra</h2>
+            {orderBD ? (
+                <div>
+                <p>Gracias por tu compra, {orderBD.email}</p>
+                <ul>
+                    {orderBD.products.map((item, index) => (
+                    <li key={index}>{item.name} - Cantidad: {item.quantity}</li>
+                    ))}
+                </ul>
+                </div>
+            ) : (
+                <p>Cargando orden...</p>
+            )}
         </>
     )
 }
